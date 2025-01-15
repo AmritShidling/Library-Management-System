352e18eed806dc25cc4aa5081444358ea48cf37b
amritlshidling <amritlshidling@gmail.com>
2025-01-15 21:54:29 +0000

    Initial commit: Complete library management system with Docker support

---
+++ borrowing-service/src/main/java/com/ericsson/library/borrowing_service/config/BorrowingServiceConfig.java
@@ -0,0 +1,26 @@
+package com.ericsson.library.borrowing_service.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app.borrow")
+class BorrowingProperties {
+    private int maxBooksPerUser = 5;
+    private int loanPeriodDays = 14;
+}
+
+@Configuration
+class RestTemplateConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
